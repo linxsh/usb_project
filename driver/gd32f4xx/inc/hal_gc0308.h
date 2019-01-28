c0d14fa24db7022cc3d3589026bfaa2d74723ecc
linxsh <linxsh@nationalchip.com>
2019-01-28 08:51:30 +0800

    add gd32f4xx

--- driver/gd32f4xx/inc/hal_gc0308.h
+++ driver/gd32f4xx/inc/hal_gc0308.h
@@ -7,7 +7,7 @@ extern void hal_gc0308_init(void);
 extern void hal_gc0308_deinit(void);
 extern int  hal_gc0308_set(CMD_SET_CAMERA_T cmd, void *args);
 extern int  hal_gc0308_enable(void);
-extern int  hal_gc0308_wait(void);
+extern int  hal_gc0308_finish(void);
 extern int  hal_gc0308_config_buffer(uint8_t *buf, uint32_t len);
 
 #endif
---
+++ driver/gd32f4xx/inc/hal_systick.h
@@ -0,0 +1,7 @@
+#ifndef __HAL_SYS_TICK_H__
+#define __HAL_SYS_TICK_H__
+
+extern int hal_systick_init   (void);
+extern int hal_systick_delayms(void);
+
+#endif
