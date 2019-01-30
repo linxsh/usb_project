2e719ed09d26a358e84c6862cad5e3972d55de2d
linxsh <linxsh@nationalchip.com>
2019-01-30 11:27:55 +0800

    fix uart finger cmd

--- pc-tools/uartfinger/cmd.c
+++ pc-tools/uartfinger/cmd.c
@@ -1,20 +1,48 @@
 #include "cmd.h"
+#include "uart.h"
 
-int uart_send_read_cmd(UART_FINGER_CMD *cmd)
+#define CHECK_TAG(tag) do {        \
+	if (tag != UART_FINGER_TAG_FLAG) \
+		return -1;                    \
+} while(0)
+
+int uart_send_cmd(int fdcom, UART_FINGER_CMD *cmd)
 {
+	CHECK_TAG(cmd->tag);
+
+	return port_send(fdcom, (char*)cmd, sizeof(UART_FINGER_CMD));
 }
 
-int uart_send_write_cmd(UART_FINGER_CMD *cmd)
+int uart_recv_cmd(int fdcom, UART_FINGER_CMD *cmd, int timeout_ms)
 {
+	unsigned int recv_len = 0;
+
+	while (recv_len < sizeof(UART_FINGER_CMD)) {
+		recv_len += port_recv(fdcom, (((char*)cmd) + recv_len),
+				(sizeof(UART_FINGER_CMD) - recv_len), timeout_ms);
+		if (recv_len == 0)
+			return -1;
+	}
+	CHECK_TAG(cmd->tag);
+
+	return 0;
 }
 
-int uart_recv_ack_cmd(UART_FINGER_CMD *cmd)
+int uart_send_data(int fdcom, unsigned char *buf, unsigned int len)
 {
+	return port_send(fdcom, (char*)buf, len);;
 }
 
-int uart_send_data(unsigned char *buf, unsigned int len)
+int uart_recv_data(int fdcom, unsigned char *buf, unsigned int len, int timeout_ms)
 {
-}
+	unsigned int recv_len = 0;
 
-int uart_recv_data(unsigned char *buf, unsigned int len)
-{
+	while (recv_len < len) {
+		recv_len += port_recv(fdcom, ((char *)buf + recv_len),
+				(len - recv_len), timeout_ms);
+		if (recv_len == 0)
+			return -1;
+	}
+
+	return 0;
+}
